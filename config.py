0bb077e40da8e2dd2a3b84057550f612e63eaf26
JEOL HERE! <102511853+Jeolpaul@users.noreply.github.com>
2022-09-05 16:53:50 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+from os import getenv
+
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5172114510").split()))
+API_ID = int(getenv("API_ID", ""))
+API_HASH = getenv("API_HASH", "")
+LOG_CHAT = int(getenv("LOG_CHAT", ""))
+MONGO_DB = getenv("MONGO_DB", "")
+ALIVE_IMG = getenv("ALIVE_IMG", "")
+DB_URL = getenv("DATABASE_URL", "")
