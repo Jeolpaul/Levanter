8d4a51d1dc553fb93d4c071dfc521ac22a7ef75e
JEOL HERE! <102511853+Jeolpaul@users.noreply.github.com>
2022-09-07 22:41:23 +0530

    Update hearts.py

--- levanter/hearts.py
+++ levanter/hearts.py
@@ -66,8 +66,37 @@ async def phase3(message):
         await _wrap_edit(message, repl)
         await asyncio.sleep(SLEEP)
 
-
 async def phase4(message):
+    """Big scroll"""
+    BIG_SCROLL = "🧡💛💚💙💜🖤🤎"
+    await _wrap_edit(message, joined_heart)
+    for heart in BIG_SCROLL:
+        await _wrap_edit(message, joined_heart.replace(R, heart))
+        await asyncio.sleep(SLEEP)
+
+
+async def phase5(message):
+    """Per-heart randomiser"""
+    ALL = ["❤️"] + list("🧡💛💚💙💜🤎🖤")  # don't include white heart
+
+    format_heart = joined_heart.replace(R, "{}")
+    for _ in range(5):
+        heart = format_heart.format(*random.choices(ALL, k=heartlet_len))
+        await _wrap_edit(message, heart)
+        await asyncio.sleep(SLEEP)
+
+
+async def phase6(message):
+    """Fill up heartlet matrix"""
+    await _wrap_edit(message, joined_heart)
+    await asyncio.sleep(SLEEP * 2)
+    repl = joined_heart
+    for _ in range(joined_heart.count(W)):
+        repl = repl.replace(W, R, 1)
+        await _wrap_edit(message, repl)
+        await asyncio.sleep(SLEEP)
+
+async def phase7(message):
     """Matrix shrinking"""
     for i in range(7, 0, -1):
         heart_matrix = "\n".join([R * i] * i)
